593886f2321916dedca06893c5c174c5ba46f6b8
Yashraj9673 <yashrajchaudhari48924@gmail.com>
2022-10-11 16:12:23 +0530

    initial commit

---
+++ Programs/KM_MILE.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class KM_MILE {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Kilometer: ");
+        double km = sc.nextDouble();
+        double mile = (km * 0.62137);
+        System.out.println("Miles: " + mile);
+    }
+}
