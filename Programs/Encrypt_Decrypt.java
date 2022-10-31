95207c9a5098e16ecf5bbba3fa277144db5cccc6
Yashraj9673 <yashrajchaudhari48924@gmail.com>
2022-10-31 12:50:36 +0530

    initial commit

--- Programs/Encrypt_Decrypt.java
+++ Programs/Encrypt_Decrypt.java
@@ -3,7 +3,7 @@ package Programs;
 public class Encrypt_Decrypt {
     public static void main(String[] args) {
         // Encrypting the grade
-        char grade = 'E';
+        char grade = 'F';
         grade = (char) (grade + 8);
         System.out.println(grade);
 
