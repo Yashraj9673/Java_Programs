8b44f0922a48e0aec0b433d68cc16c2e7e773c99
Yashraj9673 <yashrajchaudhari48924@gmail.com>
2022-11-04 10:11:24 +0530

    initial commit

---
+++ Programs/ASCII_Value.java
@@ -0,0 +1,14 @@
+package Programs;
+
+public class ASCII_Value {
+    public static void main(String[] args) {
+        char ch1 = 'A';
+        char ch2 = 'B';
+
+        int Ascii1 = ch1;
+        int Ascii2 = ch2;
+
+        System.out.println("The ASCII value of " + ch1 + " is: " + Ascii1);
+        System.out.println("The ASCII value of " + ch2 + " is: " + Ascii2);
+    }
+}
