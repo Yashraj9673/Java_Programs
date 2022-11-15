d6b7b953e495a60e54478e139d311623df2389da
Yashraj9673 <yashrajchaudhari48924@gmail.com>
2022-11-15 23:58:10 +0530

    initial commit

--- Programs/Fibo_Series.java
+++ Programs/Fibo_Series.java
@@ -1,9 +1,15 @@
 package Programs;
 
+import java.util.*;
+
 public class Fibo_Series {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of series: ");
+        int n = sc.nextInt();
+
         int n1 = 0, n2 = 1;
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= n; i++) {
             System.out.print(n1 + " ");
             int n3 = n1 + n2;
             n1 = n2;
